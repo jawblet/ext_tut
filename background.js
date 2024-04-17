62000ec3994c224a8662102458f149c9071dd133
jawblet <juliabell021@gmail.com>
2024-04-17 12:38:45 -0400

    playing w basic localstorage functionality

--- background.js
+++ background.js
@@ -1,3 +1,14 @@
-chrome.runtime.onInstalled.addListener(async () => {
-        console.log("HEY!");
+/**
+ * Store installation date in local storage 
+ */
+chrome.runtime.onInstalled.addListener(() => {
+        chrome.storage.local.get(["installed"]).then((result) => {
+                if(result.value == undefined) {
+                        var today = new Date().toLocaleString();
+                        chrome.storage.local.set({ "installed": today }).then(() => {
+                                console.log("Value is set");
+                        });
+                        }
+        });
 });
+
--- popup.js
+++ popup.js
@@ -1,2 +1,23 @@
 console.log('This is a popup!');
 
+
+/**
+ * To get items from local storage, need to use promises/callbacks
+ */
+function GetDateFromLocalStorage() {
+        return new Promise(function(resolve, reject) {
+            chrome.storage.local.get({installed: ''}, function(items) {
+                resolve(items.installed);
+             })
+        });
+    }
+
+async function getDate() {
+        console.log("GET DATE");
+        var date;
+        date = await GetDateFromLocalStorage();
+        const full_string = "Monitoring attribution data since " + date.toString();
+        document.querySelector(".footer").innerText = full_string;
+}
+
+getDate();
