5d937c97358dfb2e668cfbda2d2c651b89efb66c
jawblet <juliabell021@gmail.com>
2024-05-05 15:54:16 -0400

    updates, trying to load sqllite

--- popup.js
+++ popup.js
@@ -1,23 +1,33 @@
-console.log('This is a popup!');
 
 
-/**
- * To get items from local storage, need to use promises/callbacks
- */
-function GetDateFromLocalStorage() {
-        return new Promise(function(resolve, reject) {
-            chrome.storage.local.get({installed: ''}, function(items) {
-                resolve(items.installed);
-             })
-        });
-    }
+var file = "file:///Users/juliabell/Library/Application%20Support/Google/Chrome/Default/Conversions"
 
-async function getDate() {
-        console.log("GET DATE");
-        var date;
-        date = await GetDateFromLocalStorage();
-        const full_string = "Monitoring attribution data since " + date.toString();
-        document.querySelector(".footer").innerText = full_string;
-}
+fetch(file)
+  .then((res) => res.text())
+  .then((text) => {
+    console.log(text)
+   })
+  .catch((e) => console.error(e));
+  
 
-getDate();
+
+// /**
+//  * To get items from local storage, need to use promises/callbacks
+//  */
+// function GetDateFromLocalStorage() {
+//         return new Promise(function(resolve, reject) {
+//             chrome.storage.local.get({installed: ''}, function(items) {
+//                 resolve(items.installed);
+//              })
+//         });
+//     }
+
+// async function getDate() {
+//         console.log("GET DATE");
+//         var date;
+//         date = await GetDateFromLocalStorage();
+//         const full_string = "Monitoring attribution data since " + date.toString();
+//         document.querySelector(".footer").innerText = full_string;
+// }
+
+// getDate();
